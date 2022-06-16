1127f34aa7ddb494da6a4febf3294635caf43657
CAEL0 <kjongmin26@gmail.com>
2022-06-16 12:30:58 +0900

    upload

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"os"
+	"strings"
 
-	"github.com/CAEL0/learn-go/accounts"
+	"github.com/CAEL0/learn-go/scrapper"
+	"github.com/labstack/echo"
 )
 
+const fileName string = "jobs.csv"
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func handleScrape(c echo.Context) error {
+	defer os.Remove(fileName)
+	term := strings.ToLower(strings.TrimSpace(c.FormValue("term")))
+	scrapper.Scrape(term)
+	return c.Attachment(fileName, fileName)
+}
+
 func main() {
-	account := accounts.NewAccount("CAELO")
-	fmt.Println(account)
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":1323"))
 }
